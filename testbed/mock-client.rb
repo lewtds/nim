e5477c530a18ccf67d4fdfe77bae98f9ac8612ac
Hoàng Minh Thắng <p@banphim.net>
2013-10-15 12:02:11 +0700

    add :on_string_committed signal

--- testbed/mock-client.rb
+++ testbed/mock-client.rb
@@ -9,4 +9,14 @@ server.default_iface = 'nim.server.InputContext'
 
 server.introspect
 
+server.on_signal("on_string_committed") do |str|
+  puts "String committed: #{str}"
+end
+
 server.send_key 'yay'
+
+main = DBus::Main.new
+
+main << session_bus
+
+main.run
--- testbed/mock.rb
+++ testbed/mock.rb
@@ -5,7 +5,9 @@ class Nim < DBus::Object
   dbus_interface "nim.server.InputContext" do
     dbus_method :send_key, "in key:s" do |key|
       puts "This key got pressed: #{key}"
+      self.on_string_committed "I got this key #{key} so I sing a song"
     end
+    dbus_signal :on_string_committed, "str:s"
   end
 end
 
